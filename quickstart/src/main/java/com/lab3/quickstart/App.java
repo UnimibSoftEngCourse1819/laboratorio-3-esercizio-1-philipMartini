12d939e0b15e408a1e2b8cb6c76f8325b0065eb6
philipMartini <45294280+philipMartini@users.noreply.github.com>
2019-01-13 14:13:03 +0100

    Add files via upload

---
+++ quickstart/src/main/java/com/lab3/quickstart/App.java
@@ -0,0 +1,13 @@
+package com.lab3.quickstart;
+
+import com.google.gson.Gson;
+
+public class App {
+
+	public static void main(String[] args) {
+		Gson gson = new Gson();
+		System.out.println("Hello Wolrd!");
+
+	}
+
+}
